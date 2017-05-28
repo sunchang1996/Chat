bbb150e256b6df29130d992e0a62e7ff0db169fb
sunchang1996 <1029841435@qq.com>
2017-05-28 15:21:05 +0800

    实名聊天

--- server.js
+++ server.js
@@ -16,12 +16,16 @@ let server = require('http').createServer(app);
 // 创建一个IO，并且把server作为参数传入进来
 let io = require('socket.io')(server);
 io.on('connection',function (socket) {
+    let username;
     // 当服务器端接收到客户端的消息之后执行回调函数 msg就是对应的消息
     socket.on('message',function (msg) {
-        console.log(msg);
         // 广播给所有人
-        io.emit('message',msg);
-
+        if(username){
+            io.emit('message',{username,content:msg,createAt:new Date().toLocaleString()})
+        }else {
+            username= msg ;
+            io.emit('message',{username:'系统',content:`欢迎${username}来到聊天室`,createAt:new Date().toLocaleString()});
+        }
     })
 });
 
