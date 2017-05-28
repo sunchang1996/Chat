fd8577001dcc020974ad544720047f196c10ed9d
sunchang1996 <1029841435@qq.com>
2017-05-28 16:10:22 +0800

    实现@私发消息

--- server.js
+++ server.js
@@ -15,15 +15,30 @@ app.get('/',function (req, res) {
 let server = require('http').createServer(app);
 // 创建一个IO，并且把server作为参数传入进来
 let io = require('socket.io')(server);
+//
+let sockets= {};
 io.on('connection',function (socket) {
     let username;
     // 当服务器端接收到客户端的消息之后执行回调函数 msg就是对应的消息
     socket.on('message',function (msg) {
         // 广播给所有人
         if(username){
-            io.emit('message',{username,content:msg,createAt:new Date().toLocaleString()})
+            let reg = /@([^\s]+) (.+)/;
+            let result =  msg.match(reg);
+            if(result){ // 私聊
+                //得到用户名
+                let toUser = result[1];
+                // 得到内容
+                let content = result[2];
+                socket[toUser].send({username,content,createAt:new Date().toLocaleString()})
+                sockets[toUser].send({username,content,createAt:new Date().toLocaleString()})
+            }else {
+                 io.emit('message',{username,content:msg,createAt:new Date().toLocaleString()})
+            }
         }else {
             username= msg ;
+            // 建立用户名和socket对象的对应关系
+            sockets[username] = socket;
             io.emit('message',{username:'系统',content:`欢迎${username}来到聊天室`,createAt:new Date().toLocaleString()});
         }
     })
